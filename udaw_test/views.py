7c5645dbb4fed2d347cc5146b3cd740bc4f12585
udaw <udaw@mail.ua>
2016-09-25 15:52:31 +0300

    ticket:1 with index view and  index template updated to use model

--- udaw_test/views.py
+++ udaw_test/views.py
@@ -1,9 +1,11 @@
 """"Simple view."""
 from django.shortcuts import render
+from models import Person
 
 # Create your views here.
 
 
 def index(request):
     """Person view."""
-    return render(request, 'index.html')
+    person = Person.objects.get(pk=1)
+    return render(request, 'index.html', {'person': person})
