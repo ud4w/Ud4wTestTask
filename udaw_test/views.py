b7c8866825cab1b47a5c74ab4cfe5e9dbda03e2f
udaw <udaw@mail.ua>
2016-09-25 11:31:04 +0300

    ticket:1 with hadr coded template

--- udaw_test/views.py
+++ udaw_test/views.py
@@ -1,11 +1,12 @@
 """"Simple view."""
 from django.shortcuts import render
-from models import Person
+# from models import Person
+
 # Create your views here.
 
 
 def index(request):
     """Person view."""
-    person = Person.objects.get(pk=1)
-    context = {'person': person}
-    return render(request, 'index.html', context)
+    # person = Person.objects.get(pk=1)
+    # context = {'person': person}
+    return render(request, 'index.html')
