91fd8a682b83547681b6268e7b5574b10bf8fd3a
udaw <udaw@mail.ua>
2016-09-25 10:20:22 +0300

    ticket:1 looks like done with templates, tests for view and view and migrations + random db name

--- Ud4wTestTask/settings.py
+++ Ud4wTestTask/settings.py
@@ -62,7 +62,7 @@ WSGI_APPLICATION = 'Ud4wTestTask.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'test_db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'rdbname.sqlite3'),
     }
 }
 
