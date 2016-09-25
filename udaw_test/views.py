8fec05c46f51bd2825ed3f468a177a80184a5c86
udaw <udaw@mail.ua>
2016-09-25 12:03:48 +0300

    ticket:1 according to pep now

--- udaw_test/views.py
+++ udaw_test/views.py
@@ -1,12 +1,9 @@
 """"Simple view."""
 from django.shortcuts import render
-# from models import Person
 
 # Create your views here.
 
 
 def index(request):
     """Person view."""
-    # person = Person.objects.get(pk=1)
-    # context = {'person': person}
     return render(request, 'index.html')
